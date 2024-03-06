3b2745aebaa8ae5c47e311411c001e714386ba5c
Vrutansh Patel <vrutanshpatel01@gmail.com>
2024-03-06 14:17:45 +0530

    nav comps

---
+++ src/components/Fundamentals.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Fundamentals() {
+  return (
+    <div>Fundamentals</div>
+  )
+}
+
+export default Fundamentals
---
+++ src/components/Overview.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Overview() {
+  return (
+    <div className='w-[full] h-[77vh] rounded-xl bg-orange-700'>Overview</div>
+  )
+}
+
+export default Overview
