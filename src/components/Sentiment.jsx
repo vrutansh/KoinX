efb9ba37e64776c60ee9657e092d5809f1e84746
Vrutansh Patel <vrutanshpatel01@gmail.com>
2024-03-07 15:32:43 +0530

    miscellenous

---
+++ src/components/Sentiment.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function Sentiment() {
+  return (
+   <>
+     <div className='bg-white h-full w-full overflow-hidden rounded-xl p-4 '>
+      <h1 className='text-4xl font-semibold '>Sentiment</h1>
+      <h1 className='text-zinc-600 text-xl'>Key Events</h1>
+
+         
+     </div>
+   </>
+  )
+}
+
+export default Sentiment
