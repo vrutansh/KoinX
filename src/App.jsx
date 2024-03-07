260b23d9771fca5388eed032c9ef457530fbeeb9
Vrutansh Patel <vrutanshpatel01@gmail.com>
2024-03-07 15:31:29 +0530

    app

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import Navbar from './components/Navbar'
 import Bitcoinmain from './components/Bitcoinmain'
+import Overview from './components/Overview'
 
 
 
 const App = () => {
   return (
-    <div className='bg-zinc-100'>
+    <div className='bg-zinc-100 w-full h-screen'>
       <Navbar/>
       <h1 className='mx-11 text-zinc-600 inline-block '>Cryptocurriences <span className=' inline-block font-semibold text-md '>&gt;&gt; Bitcoin</span></h1>
       <Bitcoinmain/>
